06339bf46604eb987ebad6da1e0f12320a7edd5d
Monisola <khairaah@gmail.com>
2023-09-27 21:08:51 +0000

    pascals triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,6 +5,11 @@
 
 
 def pascal_triangle(n):
+ """
+    Returns a list of integers
+    representing the Pascal Triangle of n
+    returns empty list if n <= 0
+    """
     if n <= 0:
         return []
 
