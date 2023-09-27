73ba7dbba6828e23e3c33e51e6cf89912364c2f6
Monisola <khairaah@gmail.com>
2023-09-27 20:51:31 +0000

    Pascals Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+0-pascal_triangle
+"""
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = []
+
+    for i in range(n):
+        row = [1]  # First element in each row is always 1
+
+        if triangle:
+            last_row = triangle[-1]
+
+            for j in range(len(last_row) - 1):
+                row.append(last_row[j] + last_row[j + 1])
+
+            row.append(1)  # Last element in each row is always 1
+
+        triangle.append(row)
+
+    return triangle
+
